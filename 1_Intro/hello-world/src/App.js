905aeffd0f81b45a62e35f0c57f4ef2b9fe705a3
vini230403 <vini2003.melo@gmail.com>
2024-12-16 12:31:46 -0300

    testeNote

--- 1_Intro/hello-world/src/App.js
+++ 1_Intro/hello-world/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <p>Meu primeiro teste em React</p>
+      <p>Meu primeiro teste em react</p>
       <p2>Olá</p2>
     </div>
   );
